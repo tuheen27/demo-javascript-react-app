13090fd962af2a0d55e2573a2e586acd1e1da1c1
Tuhin <145905935+tuheen27@users.noreply.github.com>
2025-11-04 19:43:47 +0530

    changes made in this project where we  are update the pakage of this project

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-// import the React Test Renderer
-import { create } from "react-test-renderer";
-// test card state renders
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = createRoot(div);
+  root.render(<App />);
+  root.unmount();
 });
